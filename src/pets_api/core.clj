cacfd4e80df301d0a373e44591533d52fffe8485
Timoteo Ponce <ponce.timoteo@gmail.com>
2025-08-29 06:10:32 -0400

    Adds support for json-response and logging

--- src/pets_api/core.clj
+++ src/pets_api/core.clj
@@ -1,13 +1,32 @@
 (ns pets-api.core
   (:gen-class)
-  (:require [compojure.core :refer :all]
+  (:require [clojure.tools.logging :as log]
+            [compojure.core :refer :all]
             [compojure.route :as route]
-            [ring.adapter.jetty :refer [run-jetty]]))
+            [ring.adapter.jetty :refer [run-jetty]]
+            [ring.middleware.json :refer [wrap-json-response]]))
 
+;; log requests
+(defn wrap-logging [handler]
+  (fn [request]
+    (log/info (str "Request: "
+                   (:request-method request ) " "
+                   (:uri request)
+                   (when-let [query (:query-string request)]
+                     (str "?" query))))
+    (handler request)))
+
+;; Define routes
 (defroutes app-routes
-  (GET "/" [] {:status 200
-               :headers {"Content-Type" "application/json"}
-               :body "{\"message\": \" Hello, World!\"}"}))
+  (GET "/" [] {:status 200 :body {:message "Helloo World"}}))
 
+;; Wrap the routes with JSON middleware
+(def app
+  (-> app-routes
+      wrap-logging
+      wrap-json-response))
+
+;; Main function to start the Jetty server
 (defn -main []
-  (run-jetty app-routes {:port 3000 :join? false}))
+  (run-jetty app {:port 3000 :join? false}))
+
